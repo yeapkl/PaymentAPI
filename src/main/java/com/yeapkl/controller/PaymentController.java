86279869f1c4ff2d3d375065e88986c934250af6
yeap kwan lin <freedom.ykl@gmail.com>
2021-09-05 14:48:17 +0800

    remove response file

--- src/main/java/com/yeapkl/controller/PaymentController.java
+++ src/main/java/com/yeapkl/controller/PaymentController.java
@@ -1,6 +1,6 @@
 package com.yeapkl.controller;
 
-import com.yeapkl.model.PaymentDetailsResponse;
+import com.yeapkl.entity.PaymentDetails;
 import com.yeapkl.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PaymentController {
 
@@ -16,26 +18,26 @@ public class PaymentController {
     PaymentService paymentService;
 
     @GetMapping(path="/getByCustomerId")
-    public PaymentDetailsResponse getByCustomerId(@RequestParam(value = "customerId") int customerId,
-                                                  @RequestParam(defaultValue = "0") int page,
-                                                  @RequestParam(defaultValue = "5") int size){
+    public List<PaymentDetails> getByCustomerId(@RequestParam(value = "customerId") int customerId,
+                                                @RequestParam(defaultValue = "0") int page,
+                                                @RequestParam(defaultValue = "5") int size){
         Pageable pageable = PageRequest.of(page, size);
-        return new PaymentDetailsResponse(paymentService.getByCustomerId(customerId, pageable));
+        return paymentService.getByCustomerId(customerId, pageable);
     }
 
     @GetMapping(path="/getByAccountNumber")
-    public PaymentDetailsResponse getByAccountNumber(@RequestParam(value = "accountNumber") String accountNumber,
+    public List<PaymentDetails> getByAccountNumber(@RequestParam(value = "accountNumber") String accountNumber,
                                                      @RequestParam(defaultValue = "0") int page,
                                                      @RequestParam(defaultValue = "5") int size){
         Pageable pageable = PageRequest.of(page, size);
-        return new PaymentDetailsResponse(paymentService.getByAccountNumber(accountNumber, pageable));
+        return paymentService.getByAccountNumber(accountNumber, pageable);
     }
 
     @GetMapping(path="/getByDescription")
-    public PaymentDetailsResponse getByDescription(@RequestParam(value = "description") String description,
+    public List<PaymentDetails> getByDescription(@RequestParam(value = "description") String description,
                                                    @RequestParam(defaultValue = "0") int page,
                                                    @RequestParam(defaultValue = "5") int size){
         Pageable pageable = PageRequest.of(page, size);
-        return new PaymentDetailsResponse(paymentService.getByDescription(description, pageable));
+        return paymentService.getByDescription(description, pageable);
     }
 }
